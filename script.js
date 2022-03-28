e41fd61598e93315e23b6a8a87d02b0cb45faa28
Anousone <aninthirath@gmail.com>
2022-03-28 16:14:50 -0600

    updating JS

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 var apiKey = "4304f4875aa2cd2aeb35b9fea64bc893";
 var today = moment().format("L");
 var searchHistoryList = [];
+
+function currentCondition(city) {
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+
+  fetch(apiURL).then(function (response) {
+    response.json().then(function (data) {
+      displayWeather(data, city);
+    });
+  });
+}
